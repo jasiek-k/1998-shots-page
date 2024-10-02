4e043e5e8858544f4d49a53f7f96422ce6a872a5
jasiek-k <janek9805@gmail.com>
2024-10-03 00:50:14 +0200

    images fixes

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -165,8 +165,8 @@ export const gunsData: ISession = {
   title: "guns guns guns",
   about:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
-  heroPhoto: "",
-  heroPhotoMobile: "",
+  heroPhoto: "/images/guns/guns_banner.jpg",
+  heroPhotoMobile: "/images/guns/guns_banner_mobile.jpg",
   photos: [
     {
       type: "group" as EPhotoType.Group,
