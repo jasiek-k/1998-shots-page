88e183071ec61c6dcccc6465910cd9fdda630ac7
jasiek-k <janek9805@gmail.com>
2025-11-07 23:54:33 +0100

    add intersection observer handling on photos page

--- src/app/photos/ViewAllSection.tsx
+++ src/app/photos/ViewAllSection.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
 import { SessionTile, Title } from "@components";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import sessions from "@/app/mock";
+import { useIsMobile } from "@/hooks";
 
 const PageDetails = () => (
   <div className="flex flex-col justify-between px-4 md:px-0">
@@ -18,13 +22,65 @@ const PageDetails = () => (
 );
 
 const ViewAllSection = () => {
+  const [firstFullyVisible, setFirstFullyVisible] = useState(null);
+  const debounceTimeout = useRef<any>(null);
+  const refs = useRef<any>([]);
+  const isMobile = useIsMobile();
   const content = useMemo(() => Object.values(sessions).reverse(), []);
+
+  useEffect(() => {
+    if (!isMobile) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      entries => {
+        if (debounceTimeout.current) {
+          clearTimeout(debounceTimeout.current);
+        }
+
+        debounceTimeout.current = setTimeout(() => {
+          const fullyVisible = entries
+            .filter(entry => entry.intersectionRatio === 1)
+            .map(entry => entry.target);
+
+          if (fullyVisible.length > 0) {
+            const firstVisible = fullyVisible[0];
+            const index = refs.current.findIndex((el: any) => el === firstVisible);
+
+            setFirstFullyVisible(prev => (prev !== index ? index : prev));
+          }
+        }, 100);
+      },
+      { threshold: [1.0] },
+    );
+
+    refs.current.forEach((el: any) => {
+      if (el) {
+        observer.observe(el);
+      }
+    });
+
+    return () => {
+      observer.disconnect();
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
+    };
+  }, [isMobile]);
 
   return (
     <div className="grid md:grid-cols-3 gap-y-8 md:gap-x-6 md:gap-y-6">
       <PageDetails />
-      {content.map(item => (
-        <SessionTile key={item.id} {...item} />
+      {content.map((item, index) => (
+        <SessionTile
+          ref={element => {
+            refs.current[index] = element;
+          }}
+          isHighlighted={index === firstFullyVisible}
+          key={item.id}
+          {...item}
+        />
       ))}
     </div>
   );
--- src/components/SessionTile.tsx
+++ src/components/SessionTile.tsx
@@ -1,39 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { EContainerRatio, RatioContainer, ResponsiveImage } from "@components";
 import clsx from "clsx";
 import Link from "next/link";
+import { forwardRef } from "react";
 
 import type { ISession } from "@/app/types";
 import { formatYear } from "@/app/utils";
 
 interface IProps extends ISession {
   className?: string;
+  isHighlighted: boolean;
 }
 
-export const SessionTile = ({
-  id,
-  href,
-  year,
-  title,
-  subtitle,
-  heroSecondaryMobile,
-}: IProps) => (
-  <Link href={href} className="flex flex-col">
-    <RatioContainer className="sessionItem" variant={EContainerRatio.SessionItem}>
-      <ResponsiveImage
-        desktop={{ src: heroSecondaryMobile, width: 540, height: 540 }}
-        mobile={{ src: heroSecondaryMobile, width: 375, height: 375 }}
-      />
-    </RatioContainer>
-    <div className="flex flex-col px-4 md:px-0">
-      <p className="pt-3 pb-4">
-        <span className="font-normal text-xxs">ISSUE NO. {id}</span>
-        <span className="font-light text-xxxs mx-1">X</span>
-        <span className="font-normal text-xxs">YEAR {formatYear(year)}</span>
-      </p>
-      <p className="inline-block m-0 uppercase gap-x-2 items-baseline leading-base">
-        <span className={clsx("text-xmd font-normal", subtitle && "mr-2")}>{title}</span>
-        {subtitle && <span className="text-xxs font-light">{subtitle}</span>}
-      </p>
-    </div>
-  </Link>
+export const SessionTile = forwardRef(
+  (
+    { id, href, year, title, subtitle, heroSecondaryMobile, isHighlighted }: IProps,
+    ref: any,
+  ) => (
+    <Link href={href} ref={ref} className="flex flex-col">
+      <RatioContainer className="sessionItem" variant={EContainerRatio.SessionItem}>
+        {isHighlighted ? (
+          <div className="bg-off-white w-full h-full"></div>
+        ) : (
+          <ResponsiveImage
+            desktop={{ src: heroSecondaryMobile, width: 540, height: 540 }}
+            mobile={{ src: heroSecondaryMobile, width: 375, height: 375 }}
+          />
+        )}
+      </RatioContainer>
+      <div className="flex flex-col px-4 md:px-0">
+        <p className="pt-3 pb-4">
+          <span className="font-normal text-xxs">ISSUE NO. {id}</span>
+          <span className="font-light text-xxxs mx-1">X</span>
+          <span className="font-normal text-xxs">YEAR {formatYear(year)}</span>
+        </p>
+        <p className="inline-block m-0 uppercase gap-x-2 items-baseline leading-base">
+          <span className={clsx("text-xmd font-normal", subtitle && "mr-2")}>
+            {title}
+          </span>
+          {subtitle && <span className="text-xxs font-light">{subtitle}</span>}
+        </p>
+      </div>
+    </Link>
+  ),
 );
+
+SessionTile.displayName = "SessionTile";
